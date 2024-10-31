4571985b0a3fccd7922ed28a65b53520b56cdeb6
KLG <kucsen2003@gmail.com>
2024-10-31 17:18:32 -0600

    eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,13 @@
 import js from '@eslint/js';
-import { FlatCompat } from '@eslint/eslintrc';
-
-const compat = new FlatCompat();
 
 export default [
-  js.configs.recommended,
-  ...compat.extends('eslint:recommended'),
   {
     ignores: ['node_modules'],
+  },
+  js.configs.recommended, // Configuración recomendada de ESLint para JS
+  {
     rules: {
-      // Aquí puedes agregar reglas personalizadas
-      'no-unused-vars': 'warn',
+      'no-unused-vars': 'warn', // Reglas personalizadas
       'no-console': 'off',
     },
   },
